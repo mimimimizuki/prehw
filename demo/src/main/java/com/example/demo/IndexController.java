71dee650b85a9e5e020b3d15ce97ec69719f88df
mimimimizuki <mizuki0327.5@gmail.com>
2019-08-11 11:59:14 +0000

    change text, but cannot see this...

--- demo/src/main/java/com/example/demo/IndexController.java
+++ demo/src/main/java/com/example/demo/IndexController.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 @Controller
 @RequestMapping("/")
-public class ViewController {
-    @GetMapping
-    public String view(){
-        return "view";
+class IndexController {
+    @RequestMapping(name = "/", method = RequestMethod.GET);
+    public String index(){
+        return "index";
     }
 }

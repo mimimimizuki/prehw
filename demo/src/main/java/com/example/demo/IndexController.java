ff8cf36bfb31db9ceb5a9c7eaa96d52ac215ac96
mimimimizuki <mizuki0327.5@gmail.com>
2019-08-12 09:55:55 +0000

    temporary submission.

--- demo/src/main/java/com/example/demo/IndexController.java
+++ demo/src/main/java/com/example/demo/IndexController.java
@@ -3,12 +3,11 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
 class IndexController {
-    @RequestMapping(name = "/", method = RequestMethod.GET);
+    @GetMapping
     public String index(){
         return "index";
     }
